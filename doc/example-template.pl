bd1803128bc20ee8dee9039be586296a016990d0
Amaranese <32201849+Amaranese@users.noreply.github.com>
2021-10-14 10:05:27 +0200

    Create example-template.pl

---
+++ doc/example-template.pl
@@ -0,0 +1,35 @@
+=begin pod
+
+=TITLE Short title for the example
+
+=AUTHOR Who wrote this example? (either real name or nick)
+
+DESCRIPTION
+
+=head1 What's interesting here?
+
+=item feature1
+=item feature2
+=item ...
+
+=head1 More
+
+L<link-for-more-information>
+
+=head1 Features used
+
+=item C<name> - L<url>
+
+If the example solves an interesting problem with a known result, mention
+what the expected result of the computation is.
+
+=end pod
+
+CODE
+
+# Local Variables:
+# mode: perl6
+# indent-tabs-mode: nil
+# perl6-indent-offset: 4
+# End:
+# vim: expandtab shiftwidth=4 ft=perl6
